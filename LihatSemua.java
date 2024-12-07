7e85bdb0b4aea8e1371f643cc5adeba59f27214c
riskasftr26 <rizani4366@gmail.com>
2024-12-07 23:49:16 +0800

    F1D022088_Rafli Rizani_Progress 3

--- LihatSemua.java
+++ LihatSemua.java
@@ -4,13 +4,13 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
-public class Bioskop {
+public class LihatSemua {
     private JFrame frame;
     private JPanel mainPanel;
 
     public void launchApp() {
-        frame = new JFrame("Bioskop");
-        frame.setSize(1920, 1080);
+        frame = new JFrame("LihatSemua");
+        frame.setSize(1440, 1080);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
@@ -31,7 +31,7 @@ public class Bioskop {
         // Mengambil gambar dari icon
         Image img = icon.getImage();
         // Mengubah ukuran gambar agar sesuai keinginan
-        Image imgScaled = img.getScaledInstance(1550, 320, Image.SCALE_SMOOTH);
+        Image imgScaled = img.getScaledInstance(1440, 150, Image.SCALE_SMOOTH);
         // Mengubah gambar menjadi icon
         ImageIcon iconScaled = new ImageIcon(imgScaled);
         // Membuat label dengan iconScaled
@@ -41,12 +41,12 @@ public class Bioskop {
 
         ImageIcon profile = new ImageIcon("Asset/Profil.png");
         Image imgProfile = profile.getImage();
-        Image imgProfileScaled = imgProfile.getScaledInstance(80, 80, Image.SCALE_SMOOTH);
+        Image imgProfileScaled = imgProfile.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
         ImageIcon profileScaled = new ImageIcon(imgProfileScaled);
 
         JPanel profilePanel = new JPanel(new BorderLayout());
         profilePanel.setOpaque(false);
-        profilePanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 200, 80));
+        profilePanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 50));
         JLabel profileLabel = new JLabel(profileScaled);
         profileLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         profilePanel.add(profileLabel, BorderLayout.EAST);
@@ -220,7 +220,7 @@ public class Bioskop {
         // Membuat panel utama untuk konten
         JPanel mainContentPanel = new JPanel();
         // Mengatur border dari mainContentPanel dengan padding 20px di atas
-        mainContentPanel.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
+        mainContentPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 0, 20));
         // Mengatur layout dari mainContentPanel menjadi BoxLayout dengan arah vertikal
         mainContentPanel.setLayout(new BoxLayout(mainContentPanel, BoxLayout.Y_AXIS));
         // Mengatur warna background dari mainContentPanel menjadi BLACK
@@ -235,81 +235,145 @@ public class Bioskop {
                 "asset/poster/moana2.jpg", "asset/shawshank_redemption.mp4"));
         movieList.add(new Movie("Moana 2", "......", 1994, ".....", ".....",
                 "asset/poster/moana2.jpg", "asset/shawshank_redemption.mp4"));
+        movieList.add(new Movie("Moana 2", "......", 1994, ".....", ".....",
+                "asset/poster/moana2.jpg", "asset/shawshank_redemption.mp4"));
         mainContentPanel.add(createCategoryPanel("Sedang Tayang", movieList));
-        // Menambahkan panel series ke mainContentPanel dengan data yang sudah
-        // diinputkan
-        mainContentPanel.add(createCategoryPanel("Coming Soon", movieList));
-
         // Mengembalikan mainContentPanel
         return mainContentPanel;
     }
 
-    private JPanel createCategoryPanel(String category, ArrayList<Movie> movieList) {
+    private static JPanel createCategoryPanel(String categoryTitle, ArrayList<Movie> movieList) {
         // Membuat panel untuk kategori
-        JPanel categoryPanel = new JPanel(new BorderLayout());
-        // Mengatur border dari categoryPanel dengan padding 20px
-        categoryPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        // Mengatur warna background dari categoryPanel menjadi GRAY
+        JPanel categoryPanel = new JPanel();
         categoryPanel.setBackground(Color.WHITE);
+        categoryPanel.setLayout(new BorderLayout());
 
-        // Membuat label untuk judul kategori
-        JLabel categoryLabel = new JLabel(category);
-        // Mengatur font dari categoryLabel menjadi Arial Bold 24px
-        categoryLabel.setFont(new Font("Arial", Font.BOLD, 34));
-        // Mengatur warna dari categoryLabel menjadi WHITE
+        // Membuat label untuk judul kategori (Movies atau Series)
+        JLabel categoryLabel = new JLabel(categoryTitle);
+        categoryLabel.setFont(new Font("Arial", Font.BOLD, 60));
         categoryLabel.setForeground(new Color(236, 157, 36, 255));
+        categoryLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        categoryLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 20, 0));
+        categoryPanel.add(categoryLabel, BorderLayout.NORTH);
 
-        JLabel lihatSemua = new JLabel("lihat semua >");
-        // Mengatur font dari lihatSemua menjadi Arial Bold 24px
-        lihatSemua.setFont(new Font("Arial", Font.BOLD, 24));
-
-        // Mengatur warna dari lihatSemua menjadi WHITE
-        lihatSemua.setForeground(new Color(236, 157, 36, 255));
-
-        JPanel headCategory = new JPanel(new BorderLayout());
-        headCategory.add(categoryLabel, BorderLayout.CENTER);
-        headCategory.add(lihatSemua, BorderLayout.EAST);
-        headCategory.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
-        categoryPanel.add(headCategory, BorderLayout.NORTH);
-
-        // Membuat panel untuk menampilkan daftar film
-        JPanel movieListPanel = new JPanel(new GridLayout(1, 3, 20, 20));
-        // Mengatur warna background dari movieListPanel menjadi GRAY
-        movieListPanel.setBackground(Color.WHITE);
-
-        // Menambahkan setiap film ke dalam movieListPanel
-        // JPanel moviePanel = new JPanel();
-        JPanel moviePanel = new JPanel();
-        moviePanel.setLayout(new BoxLayout(moviePanel, BoxLayout.X_AXIS)); // Mengatur layout vertikal
-        moviePanel.setBorder(BorderFactory.createEmptyBorder(0, 40, 0, 20));
-
-        for (Movie movie : movieList) {
-            // Mengambil gambar poster film
-            ImageIcon poster = new ImageIcon(movie.getImage());
-            Image posterImg = poster.getImage();
-
-            // Mengubah ukuran poster sesuai dengan ukuran yang diinginkan
-            Image posterImgScaled = posterImg.getScaledInstance(300, 400, Image.SCALE_SMOOTH);
-            ImageIcon posterScaled = new ImageIcon(posterImgScaled);
-
-            // Membuat JLabel untuk menampilkan poster
-            JLabel posterLabel = new JLabel(posterScaled);
-
-            // Menambahkan jarak antara gambar poster
-            posterLabel.setAlignmentX(Component.CENTER_ALIGNMENT); // Membuat label di tengah
-            moviePanel.add(posterLabel);
-
-            // Menambahkan sedikit jarak antar poster untuk estetika
-            moviePanel.add(Box.createHorizontalStrut(75)); // Spasi antar gambar poster (dalam px)
+        // Membuat list model untuk menampung Movies
+        DefaultListModel<Movie> filmListModel = new DefaultListModel<>();
+        for (Movie film : movieList) {
+            filmListModel.addElement(film);
         }
 
-        categoryPanel.add(moviePanel, BorderLayout.CENTER);
-        return categoryPanel;
+        // Membuat JList untuk menampilkan Movies
+        JList<Movie> filmList = new JList<>(filmListModel);
+        // Mengatur renderer untuk menampilkan Movies
+        filmList.setCellRenderer(new FilmListRenderer());
+        // Mengatur warna background dari filmList menjadi BLACK
+        filmList.setBackground(Color.WHITE);
+        // filmList.setBorder(BorderFactory.createEmptyBorder(0, , 0, 0));
+        // Mengatur layout orientation dari filmList menjadi HORIZONTAL_WRAP
+        filmList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        // Mengatur visible row count dari filmList menjadi 1
+        filmList.setVisibleRowCount(0);
+        // Menambahkan mouse listener agar filmList dapat diklik
+        filmList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Jika filmList diklik satu kali
+                if (e.getClickCount() == 1) {
+                    // Mendapatkan index dari film yang diklik
+                    int index = filmList.locationToIndex(e.getPoint());
+                    if (index != -1) {
+                        // Menampilkan detail film yang dipilih
+                        // Movie selectedFilm = filmList.getModel().getElementAt(index);
+                        // showDetailWindow(selectedFilm);
+                    }
+                }
+            }
+        });
 
+        // Membuat scroll pane untuk filmList
+        JScrollPane scrollPane = new JScrollPane(filmList);
+        // Mengatur horizontal scroll bar policy dari scrollPane menjadi NEVER
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        // Mengatur vertical scroll bar policy dari scrollPane menjadi ALWAYS
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        // Mengatur border dari scrollPane menjadi null
+        scrollPane.setBorder(null);
+        // Mengatur warna background dari viewport dari scrollPane menjadi BLACK
+        scrollPane.getViewport().setBackground(Color.BLACK);
+
+        // Menambahkan scrollPane ke categoryPanel
+        categoryPanel.add(scrollPane, BorderLayout.CENTER);
+
+        // Mengembalikan categoryPanel
+        return categoryPanel;
+    }
+
+    // Kelas untuk merepresentasikan film atau series
+    private static class FilmListRenderer extends JPanel implements ListCellRenderer<Movie> {
+        // Variabel untuk menampilkan gambar, judul, dan genre
+        private JLabel imageLabel;
+        private JLabel titleLabel;
+        private JLabel genreLabel;
+
+        // Konstruktor untuk membuat FilmListRenderer
+        public FilmListRenderer() {
+            // Mengatur layout dari FilmListRenderer menjadi BoxLayout dengan arah vertikal
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            // Mengatur ukuran dari FilmListRenderer menjadi 200 x 310
+            setPreferredSize(new Dimension(450, 600));
+            // Mengatur warna background dari FilmListRenderer menjadi BLACK
+            setBackground(Color.BLACK);
+
+            // Membuat label untuk menampilkan gambar
+            imageLabel = new JLabel();
+            // Mengatur alignment dari imageLabel menjadi di tengah
+            imageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+            // Membuat label untuk menampilkan judul
+            titleLabel = new JLabel();
+            titleLabel.setFont(new Font("Arial", Font.BOLD, 40));
+            titleLabel.setForeground(new Color(236, 157, 36, 255));
+            titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+            // Membuat label untuk menampilkan genre
+            genreLabel = new JLabel();
+            genreLabel.setFont(new Font("Arial", Font.BOLD, 20));
+            genreLabel.setForeground(new Color(236, 157, 36, 255));
+            genreLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+            // Menambahkan imageLabel, titleLabel, dan genreLabel ke FilmListRenderer
+            add(imageLabel);
+            add(Box.createVerticalStrut(5));
+            add(titleLabel);
+            add(genreLabel);
+
+            // Mengatur border dari FilmListRenderer menjadi null / kosong
+            setBorder(BorderFactory.createEmptyBorder());
+        }
+
+        // Method untuk mengatur tampilan dari setiap item di filmList
+        @Override
+        public Component getListCellRendererComponent(JList<? extends Movie> list, Movie film, int index,
+                boolean isSelected, boolean cellHasFocus) {
+            // Mengatur gambar, judul, dan genre dari film
+            imageLabel.setIcon(new ImageIcon(
+                    new ImageIcon(film.getImage()).getImage().getScaledInstance(370, 500, Image.SCALE_SMOOTH)));
+            titleLabel.setText(film.getTitle());
+            genreLabel.setText(film.getGenre());
+
+            // Mengatur warna background dari FilmListRenderer berdasarkan apakah item
+            // tersebut dipilih atau tidak
+            if (isSelected) {
+                setBackground(Color.DARK_GRAY);
+            } else {
+                setBackground(Color.WHITE);
+            }
+            return this;
+        }
     }
 
     public static void main(String[] args) {
-        Bioskop app = new Bioskop();
+        LihatSemua app = new LihatSemua();
         app.launchApp();
     }
 }
